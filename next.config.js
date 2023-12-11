c0e8cf9275ec8861e3a4015dd0f8f3980ff9cc03
open-book29 <anujmaurya704@gmail.com>
2023-12-11 15:03:42 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'http',
+        hostname: 'uhdtv.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mango.blender.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'download.blender.org',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
