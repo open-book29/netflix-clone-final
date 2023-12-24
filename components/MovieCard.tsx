311c88451618c7bf36f915f11fae7f1936124c9b
open-book29 <anujmaurya704@gmail.com>
2023-12-24 14:17:05 +0530

    fixes bugs

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
-import { useRouter } from "next/router"
-import {BsFillPlayFill} from "react-icons/bs"
+import { useRouter } from "next/router";
+import { BsFillPlayFill } from "react-icons/bs";
 import { BiChevronDown } from "react-icons/bi";
 import FavoriteButton from "./FavoriteButton";
 import useInfoModalStore from "@/hooks/useInfoModalStore";
@@ -11,12 +11,13 @@ interface MovieCardProps {
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
-  const router = useRouter()
-  const {openModal} = useInfoModalStore()
+  const router = useRouter();
+  const { openModal } = useInfoModalStore();
 
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <Image
+        onClick={() => openModal(data?.id)}
         className="cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]"
         src={data.thumbnailUrl}
         alt="Thumbnail"
@@ -26,7 +27,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
       />
       <div className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100">
         <Image
-        onClick={() => openModal(data?.id)}
+          onClick={() => openModal(data?.id)}
           className="cursor-pointer object-cover transition duration shadow-xl rounded-t-md w-full h-[12vw]"
           src={data.thumbnailUrl}
           alt="Thumbnail"
@@ -40,14 +41,22 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
               onClick={() => router.push(`/watch/${data?.id}`)}
               className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
             >
-                <BsFillPlayFill size={30} />
+              <BsFillPlayFill size={30} />
             </div>
             <FavoriteButton movieId={data?.id} />
-            <div onClick={() => openModal(data?.id)} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
-              <BiChevronDown className="text-white group-hover/item:text-neutral-300" size={30} />
+            <div
+              onClick={() => openModal(data?.id)}
+              className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300"
+            >
+              <BiChevronDown
+                className="text-white group-hover/item:text-neutral-300"
+                size={30}
+              />
             </div>
           </div>
-          <p className="text-green-400 font-semibold mt-4">New <span className="text-white">2023</span></p>
+          <p className="text-green-400 font-semibold mt-4">
+            New <span className="text-white">2023</span>
+          </p>
           <div className="flex flex-row mt-4 gap-2 items-center">
             <p className="text-white text-[10px] lg:text-sm">{data.duration}</p>
           </div>
